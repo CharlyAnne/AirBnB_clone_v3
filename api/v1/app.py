2987b22702ba7e9f27682ef757a4bf300684a9e7
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-05-16 15:56:33 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,47 +5,30 @@ that returns the status of the API
 """
 
 
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from models import storage
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, origins=["0.0.0.0"])
+
+
+@app.errorhandler(404)
+def not_found(exception):
+    """Handles 404 errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Method to handle teardown context"""
+    storage.close()
+
+
 if __name__ == '__main__':
-    from flask import Flask, jsonify
-    from models import storage
-    from api.v1.views import app_views
-    from flask_cors import CORS
-    from os import getenv
-
-    app = Flask(__name__)
-    app.register_blueprint(app_views)
-    resources = {
-        "/*": {
-            "origins": "0.0.0.0",
-        }
-    }
-    CORS(app, resources=resources)
-
-    @app.teardown_appcontext
-    def handleTearDown(exception):
-        """
-        A method to handle app tear down context
-        it closes the storage session
-        """
-        storage.close()
-
-    @app.errorhandler(404)
-    def not_found_error(e):
-        """
-        error handler for 404 error
-        """
-        data = {"error": "Not found"}
-        response = jsonify(data)
-        response.status_code = 404
-        return response
-
-    @app.errorhandler(400)
-    def handle_400_error(e):
-        """
-        error handler for 400 error
-        """
-        return e.description, 400
-
-    app.run(getenv('HBNB_API_HOST') or "0.0.0.0",
-            port=getenv('HBNB_API_PORT' or 5000),
-            threaded=True)
+    host = os.getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = os.getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True, debug=True)
